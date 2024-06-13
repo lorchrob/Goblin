df5f9da7c4700ba11c6d1f22f49acc8c8a2d7454
Rob Lorch <lorchrob@gmail.com>
2024-06-13 17:40:22 -0500

    Update .gitignore

---
+++ bin/main.ml
@@ -0,0 +1,11 @@
+open Sbf
+
+(* Main method *)
+let () = 
+  print_endline "Hello, world!";
+  let input_string = "" in 
+  let _ = Utils.parse input_string in 
+  ()
+  (* Step 1: Type checking *)
+  (* Step 2: Divide and conquer *)
+  (* Step 3: Print to SyGuS language *)
