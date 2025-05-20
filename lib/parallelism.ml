7ec2466331558698b777dd14e09de001774cbec3
Rob Lorch <lorchrob@gmail.com>
2025-05-20 13:09:33 -0500

    ahhhhhhhhhhhhhhh race conditions

--- lib/parallelism.ml
+++ lib/parallelism.ml
@@ -1,16 +1,15 @@
 let race3: (unit -> 'a) -> (unit -> 'a) -> (unit -> 'a) -> 'a
 = fun f1 f2 f3 ->
   let result = Atomic.make None in
-  let winner_id = Atomic.make None in
 
   let run id f =
     Domain.spawn (fun () ->
       try
         let r = f () in
-        if Atomic.compare_and_set result None (Some r) then
-          Atomic.set winner_id (Some id)
+        (* Combine result and ID into a tuple so it can be updated atomically *)
+        ignore (Atomic.compare_and_set result None (Some (id, r)))
       with exn ->
-        Printf.eprintf "Domain %d crashed with: %s\n%s\n"
+        Printf.eprintf "Domain %d crashed with: %s\n%s\n%!"
           id
           (Printexc.to_string exn)
           (Printexc.get_backtrace ());
@@ -23,20 +22,16 @@ let race3: (unit -> 'a) -> (unit -> 'a) -> (unit -> 'a) -> 'a
   let d3 = run 3 f3 in
 
   (* Wait until one finishes *)
-  let rec wait () =
-    match Atomic.get result with
-    | Some r -> r
-    | None -> Domain.cpu_relax (); wait ()
+  let (winner_id, winner) =
+    let rec wait () =
+      match Atomic.get result with
+      | Some (id, r) -> (id, r)
+      | None -> Domain.cpu_relax (); wait ()
+    in wait ()
   in
 
-  let winner = wait () in
+  Printf.printf "Function %d won the race!\n%!" winner_id;
 
-  (* Log the winner *)
-  (match Atomic.get winner_id with
-   | Some id -> Printf.printf "Function %d won the race!\n%!" id
-   | None -> Printf.printf "No winner could be determined.\n%!");
-
-  (* Join all domains *)
   Domain.join d1;
   Domain.join d2;
   Domain.join d3;
